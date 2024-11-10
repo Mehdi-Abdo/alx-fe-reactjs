feec2d9022308dd3449925a278b3a878cc5c29c4
mehdiabdelaoui0@gmail.com <mehdiabdelaoui0@gmail.com>
2024-11-10 11:09:21 +0100

    create new directory

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+      <p style ={{fontSize:'25px', color:'black'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
