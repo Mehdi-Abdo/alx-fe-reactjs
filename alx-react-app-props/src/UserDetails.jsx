de54fa629ff68c8b33236b44eb06039ae209321a
mehdiabdelaoui0@gmail.com <mehdiabdelaoui0@gmail.com>
2024-11-10 13:19:09 +0100

    aaa

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,6 +1,6 @@
 // UserDetails.jsx
 import React, { useContext } from 'react';
-import UserContext from './UserContext';
+import UserContext from './contexts/UserContext';
 
 function UserDetails() {
     const userData = useContext(UserContext);
